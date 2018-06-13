2c0fad2b4cbfb8f1ad5c9fea0f23911bef1933a1
Unknown <peterakkies@gmail.com>
2018-06-13 16:50:51 +0200

    Added bootstrap and prettified code.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+/* Add Bootstrap. */
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
