bbec8030450c79f9401a1b2933c4633978d8d390
Peter Akkies <peter@peterakkies.net>
2018-06-19 17:18:33 +0200

    Improved game explanation copy.

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -13,10 +13,10 @@ class App extends Component {
 					</header>
 					<p>This game will help you determine what you value.</p>
 					<p>
-						We start with a list of 57 values. You choose a subset of these that
-						you consider important. Then, you choose a subset of those that you
-						consider very important. Finally, you choose the three values that
-						are most important to you.
+						We start with a list of 57 values. You choose up to 25 of these that
+						you consider important. Then, you choose up to ten of <em>those</em>{' '}
+						that you consider <em>very</em> important. Finally, you choose the
+						three values that are most important to you.
 					</p>
 					<p>
 						You will find that it will become increasingly difficult to choose
