eabbf50b826b6e1a44e1027ef7bf32d65791c27f
Peter Akkies <peter@peterakkies.net>
2018-06-21 17:25:05 +0200

    Removed service worker.
    It caused caching issues in Google Chrome.

--- src/index.js
+++ src/index.js
@@ -8,4 +8,3 @@ import App from './Containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
