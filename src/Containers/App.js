afa4935132b2afd3b045946b1f0523111c049351
Peter Akkies <peter@peterakkies.net>
2018-07-04 12:07:40 +0200

    Refactored; improved ESlint config.

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,51 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
 
-import "containers/App.css";
-import Buckets from "containers/Buckets";
+import "./App.css";
+import Buckets from "./Buckets";
 
-class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<div className="container">
-					<header>
-						<h1>The Values Game</h1>
-						<h2>What's most important to you?</h2>
-						<div id="explanation">
-							<p>
-								This game will help you determine what you value most.{" "}
-								{`Here's`} how:
-							</p>
-							<ul>
-								<li>
-									<strong>Drag and drop</strong> values from one bucket to
-									another.
-								</li>
-								<li>
-									Note that you can only fit a certain number of values in each
-									bucket.
-								</li>
-								<li>
-									Work in steps. Ask: {`what's`} important? Then ask: {`what's`}{" "}
-									very important?
-								</li>
-							</ul>
-							<p>
-								Have fun! <br />
-								— Peter Akkies
-							</p>
-						</div>
-					</header>
-					<Buckets />
-					<footer>
-						Copyright 2018 <a href="https://peterakkies.net">Peter Akkies</a>.
-						Have a suggestion for improvement?{" "}
-						<a href="mailto:peter@peterakkies.net">Email me</a>!
-					</footer>
+const App = () => (
+	<div className="App">
+		<div className="container">
+			<header>
+				<h1>The Values Game</h1>
+				<h2>What&apos;s most important to you?</h2>
+				<div id="explanation">
+					<p>
+						This game will help you determine what you value most. Here&apos;s
+						how:
+					</p>
+					<ul>
+						<li>
+							<strong>Drag and drop</strong> values from one bucket to another.
+						</li>
+						<li>
+							Note that you can only fit a certain number of values in each
+							bucket.
+						</li>
+						<li>
+							Work in steps. Ask: what&apos;s important? Then ask: what&apos;s
+							very important?
+						</li>
+					</ul>
+					<p>
+						Have fun! <br />
+						— Peter Akkies
+					</p>
 				</div>
-			</div>
-		);
-	}
-}
+			</header>
+			<Buckets />
+			<footer>
+				Copyright 2018 <a href="https://peterakkies.net">Peter Akkies</a>. Have
+				a suggestion for improvement?
+				<a href="mailto:peter@peterakkies.net">Email me</a>!
+			</footer>
+		</div>
+	</div>
+);
 
 export default App;
--- src/containers/Buckets.js
+++ src/containers/Buckets.js
@@ -1,61 +1,22 @@
-import React, { Component } from 'react';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { compose } from "redux";
+import { connect } from "react-redux";
 
 /* react-dnd allows us to drag and drop values onto a bucket. */
-import { DragDropContext } from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
+import { DragDropContext } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
 
-import Bucket from '../components/Bucket';
-import { BucketList } from '../constants';
-import { assignValueToBucket } from '../store/actions';
+import Bucket from "../components/Bucket";
+import { BucketList } from "../constants";
+import { assignValueToBucket } from "../store/actions";
 
-class Buckets extends Component {
-	moveValue = (valueTitle, newBucketKey) => {
-		const valueIndex = findValueIndex(this.props.values, valueTitle);
-
-		if (!canMoveValue(this.props.values, valueTitle, newBucketKey)) {
-			alert('There is no next bucket!');
-		} else {
-			const nextValues = [
-				...this.props.values.slice(0, valueIndex),
-				{ title: valueTitle, bucketKey: newBucketKey },
-				...this.props.values.slice(valueIndex + 1)
-			];
-
-			if (checkValidValueDistribution(nextValues)) {
-				this.props.assignValueToBucket(valueIndex, valueTitle, newBucketKey);
-			} else {
-				alert(
-					'This bucket is full. Remove a value from it before adding another one.'
-				);
-			}
-		}
-	};
-
-	render() {
-		const bucketsHTML = BucketList.map((bucket) => {
-			return (
-				<Bucket
-					key={bucket.key}
-					bucketKey={bucket.key}
-					title={bucket.title}
-					values={this.props.values}
-					numValues={this.props.numValues[bucket.key]}
-					maxValues={bucket.maxValues}
-					moveValue={this.moveValue}
-				/>
-			);
-		});
-
-		return <div className="Buckets">{bucketsHTML}</div>;
-	}
-}
+const findValueIndex = (values, valueTitle) =>
+	values.findIndex((x) => x.title === valueTitle);
 
 const getBucketValueCounts = (values) => {
 	const bucketValueCounts = [];
 
-	for (let i = 0; i < BucketList.length; i++) {
+	for (let i = 0; i < BucketList.length; i += 1) {
 		bucketValueCounts.push(
 			values.filter((object) => object.bucketKey === i).length
 		);
@@ -67,7 +28,7 @@ const checkValidValueDistribution = (values) => {
 	let validDistribution = true;
 	const bucketValueCounts = getBucketValueCounts(values);
 
-	for (let i = 0; i < BucketList.length; i++) {
+	for (let i = 0; i < BucketList.length; i += 1) {
 		if (bucketValueCounts[i] > BucketList[i].maxValues) {
 			validDistribution = false;
 		}
@@ -76,21 +37,56 @@ const checkValidValueDistribution = (values) => {
 	return validDistribution;
 };
 
-const findValueIndex = (values, valueTitle) => {
-	return values.findIndex((x) => x.title === valueTitle);
-};
-
-const canMoveValue = (values, valueTitle, bucketKey) => {
+const canMoveValue = (values, valueTitle, bucketKey) =>
 	// Check whether the next bucket exists
-	return !(bucketKey > BucketList.length - 1);
-};
+	!(bucketKey > BucketList.length - 1);
 
-const mapStateToProps = (state) => {
-	return {
-		values: state.values,
-		numValues: getBucketValueCounts(state.values)
+class Buckets extends Component {
+	moveValue = (valueTitle, newBucketKey) => {
+		const { values, assignValueToBucket } = this.props;
+		const valueIndex = findValueIndex(values, valueTitle);
+
+		if (!canMoveValue(values, valueTitle, newBucketKey)) {
+			alert("There is no next bucket!");
+		} else {
+			const nextValues = [
+				...values.slice(0, valueIndex),
+				{ title: valueTitle, bucketKey: newBucketKey },
+				...values.slice(valueIndex + 1)
+			];
+
+			if (checkValidValueDistribution(nextValues)) {
+				assignValueToBucket(valueIndex, valueTitle, newBucketKey);
+			} else {
+				alert(
+					"This bucket is full. Remove a value from it before adding another one."
+				);
+			}
+		}
 	};
-};
+
+	render() {
+		const { values, numValues } = this.props;
+		const bucketsHTML = BucketList.map((bucket) => (
+			<Bucket
+				key={bucket.key}
+				bucketKey={bucket.key}
+				title={bucket.title}
+				values={values}
+				numValues={numValues[bucket.key]}
+				maxValues={bucket.maxValues}
+				moveValue={this.moveValue}
+			/>
+		));
+
+		return <div className="Buckets">{bucketsHTML}</div>;
+	}
+}
+
+const mapStateToProps = (state) => ({
+	values: state.values,
+	numValues: getBucketValueCounts(state.values)
+});
 
 export default compose(
 	connect(
